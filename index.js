8c2ede153342d52317aa9759814a20b582495e57
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-16 10:56:09 +0300

    chore(style): Исправленны некоторые ошибки отображения

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const root = document.querySelector('#app')
 root.innerHTML = data.map((item) => {
   return `
    <li class="content__boxBtn-item">
-<a class="content__boxBtn-link " href="#">
- <audio src=${item.sound}></audio>
-</a>
-</li>
-  `
+   <button class="content__boxBtn-link">
+   <audio src=${item.sound}></audio>
+   </button>
+</li>`
 })
