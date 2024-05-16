8e1f3e9dc04e0453481e2835d2209a94fc04f886
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-16 09:00:54 +0300

    feat(data.js):Добавлен массив данных

---
+++ data.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    id: 1,
+    titly: 'sun',
+    image: 'assets/summer-bg.jpg',
+    sound: 'assets/soundssummer.mp3',
+    icon: 'assets/icons/sun.svg',
+    active: false,
+  },
+  {
+    id: 2,
+    titly: 'raining',
+    image: 'assets/rainy-bg.jpg',
+    sound: 'assets/sounds/rain.mp3',
+    icon: 'assets/icons/cloud-rain.svg',
+    active: false,
+  },
+  {
+    id: 3,
+    titly: 'ssnowing',
+    image: 'assets/winter-bg.jpg',
+    sound: 'assets/sounds/winter.mp3',
+    icon: 'assets/icons/cloud-snow.svg',
+    active: false,
+  },
+]
