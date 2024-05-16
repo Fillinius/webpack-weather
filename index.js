9015570bee0c1fa8eeab7535f6a7194a9f6147c0
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-16 20:09:46 +0300

    feat(index.js): Изменение фона при переключении

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const root = document.querySelector('#app')
 
 data.forEach((item) => {
-  console.log(item)
   const li = document.createElement('li')
   const button = document.createElement('button')
   const audio = document.createElement('audio')
@@ -9,12 +8,28 @@ data.forEach((item) => {
   divSvg.style.background = `url(${item.icon})0 0 / cover no-repeat`
   divSvg.className = 'content__boxBtn-svg'
   li.className = 'content__boxBtn-item'
+  li.setAttribute('onclick', 'render(event);')
   button.className = 'content__boxBtn-link'
+  button.id = item.id
   button.style.background = `url(${item.image})0 0 / cover no-repeat`
   audio.src = item.sound
+  audio.className = item.title
   button.append(divSvg)
   button.append(audio)
   li.append(button)
-
   document.querySelector('#app').append(li)
 })
+
+function render(event) {
+  event.preventDefault()
+  const idButton = event.target.id
+  const bgWrap = document.querySelector('.wrapper')
+  const liActive = document.querySelector('.content__boxBtn-item')
+  data.forEach((x) => {
+    // логика переключения изображения
+    if (x.id === idButton) {
+      bgWrap.style.background = `url(${x.image})0 0 / cover no-repeat`
+    }
+  })
+}
+console.log(data)
