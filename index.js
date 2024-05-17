6918d6709a1d14252e34f9a4973880f5f15fff4c
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-17 16:32:21 +0300

    feat(index.js): Добавленна смена иконок.

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ data.forEach((item) => {
   const button = document.createElement('button')
   const audio = document.createElement('audio')
   const divSvg = document.createElement('div')
+  const divPause = document.createElement('div')
   divSvg.style.background = `url(${item.icon})0 0 / cover no-repeat`
   divSvg.className = 'content__boxBtn-svg'
+  divPause.className = 'content__boxBtn-pause--show'
   li.className = 'content__boxBtn-item'
   li.setAttribute('onclick', 'render(event);')
   button.className = 'content__boxBtn-link'
@@ -15,6 +17,7 @@ data.forEach((item) => {
   audio.src = item.sound
   audio.className = item.title
   button.append(divSvg)
+  button.append(divPause)
   button.append(audio)
   li.append(button)
   document.querySelector('#app').append(li)
@@ -24,24 +27,29 @@ function render(event) {
   event.preventDefault()
   const idButton = event.target.id
   const bgWrap = document.querySelector('.wrapper')
-  // const liActive = document.querySelector('.content__boxBtn-item')
-  const svgPause = document.querySelector('.content__boxBtn-svg')
+  const iconWeather = document.querySelector('.content__boxBtn-svg')
+  const iconPause = document.querySelector('content__boxBtn-pause--show')
 
   data.forEach((x) => {
     const sound = document.querySelector(`.${x.title}`)
+    const range = document.querySelector('#range')
 
-    // логика переключения изображения
     if (x.id === idButton) {
+      // логика переключения изображения
       bgWrap.style.background = `url(${x.image})0 0 / cover no-repeat`
 
       // логика включения звука
       sound.play()
 
       // логика изменения громкости
-      const range = document.querySelector('#range')
+
       range.addEventListener('change', function () {
         sound.volume = this.value
       })
+
+      // логика переключения иконки
+      iconWeather.classList.toggle('content__boxBtn-svg--show')
+      iconPause.classList.toggle('content__boxBtn-pause')
     } else {
       sound.pause()
     }
