52340b3ef0d039b50042519fdcf599fd153ef59a
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-20 10:34:51 +0300

    feat(webpack-plugins): HTML media

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -8,4 +10,53 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './public/assets/rainy-bg.jpg'),
+          to: path.resolve('./dist/public/assets'),
+        },
+        {
+          from: path.resolve(__dirname, './public/assets/summer-bg.jpg'),
+          to: path.resolve('./dist/public/assets'),
+        },
+        {
+          from: path.resolve(__dirname, './public/assets/winter-bg.jpg'),
+          to: path.resolve('./dist/public/assets'),
+        },
+        {
+          from: path.resolve(__dirname, './public/assets/icons/cloud-rain.svg'),
+          to: path.resolve('./dist/public/assets/icons'),
+        },
+        {
+          from: path.resolve(__dirname, './public/assets/icons/cloud-snow.svg'),
+          to: path.resolve('./dist/public/assets/icons'),
+        },
+        {
+          from: path.resolve(__dirname, './public/assets/icons/sun.svg'),
+          to: path.resolve('./dist/public/assets/icons'),
+        },
+        {
+          from: path.resolve(__dirname, './public/assets/icons/pause.svg'),
+          to: path.resolve('./dist/public/assets/icons'),
+        },
+        {
+          from: path.resolve(__dirname, './public/assets/sounds/rain.mp3'),
+          to: path.resolve('./dist/public/assets/sounds'),
+        },
+        {
+          from: path.resolve(__dirname, './public/assets/sounds/summer.mp3'),
+          to: path.resolve('./dist/public/assets/sounds'),
+        },
+        {
+          from: path.resolve(__dirname, './public/assets/sounds/winter.mp3'),
+          to: path.resolve('./dist/public/assets/sounds'),
+        },
+      ],
+    }),
+  ],
 }
