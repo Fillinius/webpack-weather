dc4a99eb346b339333a0ba23d148bcf57f30ea2f
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-21 18:07:49 +0300

    fix(index.js): Восстановил работоспособность
    заменил onclick на addEventListener

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ data.forEach((item) => {
   divSvg.className = 'content__boxBtn-svg'
   divPause.className = 'content__boxBtn-pause--show'
   li.className = 'content__boxBtn-item'
-  li.setAttribute('onclick', 'render(event);')
   button.className = 'content__boxBtn-link'
   button.id = item.id
   button.style.background = `url(${item.image})0 0 / cover no-repeat`
@@ -26,35 +25,37 @@ data.forEach((item) => {
   root.append(li)
 })
 
-function render(event) {
-  event.preventDefault()
-  const idButton = event.target.id
-  const bgWrap = document.querySelector('.wrapper')
-  const iconWeather = document.querySelector('.content__boxBtn-svg')
-  const iconPause = document.querySelector('content__boxBtn-pause--show')
+const liEl = document.querySelectorAll('.content__boxBtn-item')
+liEl.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault()
+    const idButton = event.target.id
+    const bgWrap = document.querySelector('.wrapper')
+    const iconWeather = document.querySelector('.content__boxBtn-svg')
+    const iconPause = document.querySelector('.content__boxBtn-pause--show')
 
-  data.forEach((x) => {
-    const sound = document.querySelector(`.${x.title}`)
-    const range = document.querySelector('#range')
+    data.forEach((x) => {
+      const sound = document.querySelector(`.${x.title}`)
+      const range = document.querySelector('#range')
+      if (x.id === idButton) {
+        // логика переключения изображения
+        bgWrap.style.background = `url(${x.image})0 0 / cover no-repeat`
 
-    if (x.id === idButton) {
-      // логика переключения изображения
-      bgWrap.style.background = `url(${x.image})0 0 / cover no-repeat`
+        // логика включения звука
+        sound.play()
 
-      // логика включения звука
-      sound.play()
+        // логика изменения громкости
 
-      // логика изменения громкости
+        range.addEventListener('change', function () {
+          sound.volume = this.value
+        })
 
-      range.addEventListener('change', function () {
-        sound.volume = this.value
-      })
-
-      // логика переключения иконки
-      iconWeather.classList.toggle('content__boxBtn-svg--show')
-      iconPause.classList.toggle('content__boxBtn-pause')
-    } else {
-      sound.pause()
-    }
+        // логика переключения иконки
+        iconWeather.classList.toggle('content__boxBtn-svg--show')
+        iconPause.classList.toggle('content__boxBtn-pause')
+      } else {
+        sound.pause()
+      }
+    })
   })
-}
+})
