881e08a23ac95244309a2ab69c38f1ea090ca54d
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-22 09:44:03 +0300

    fix(index.jx): работает переключение иконки
    Корректно работает переключение иконок

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ data.forEach((item) => {
   const button = document.createElement('button')
   const audio = document.createElement('audio')
   const divSvg = document.createElement('div')
-  const divPause = document.createElement('div')
+  // const divPause = document.createElement('div')
   divSvg.style.background = `url(${item.icon})0 0 / cover no-repeat`
   divSvg.className = `content__boxBtn-svg svg-${item.title}`
-  divPause.className = 'content__boxBtn-pause--show'
+  // divPause.className = 'content__boxBtn-pause--show'
   li.className = 'content__boxBtn-item'
   button.className = 'content__boxBtn-link'
   button.id = item.id
@@ -19,24 +19,21 @@ data.forEach((item) => {
   audio.src = item.sound
   audio.className = item.title
   button.append(divSvg)
-  button.append(divPause)
+  // button.append(divPause)
   button.append(audio)
   li.append(button)
   root.append(li)
 })
-
-const liEl = document.querySelectorAll('.content__boxBtn-item')
-liEl.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    event.preventDefault()
-    const idButton = event.target.id
-    const bgWrap = document.querySelector('.wrapper')
-    const iconWeather = document.querySelector('.content__boxBtn-svg')
-    const iconPause = document.querySelector('.content__boxBtn-pause--show')
-
-    data.forEach((x) => {
+data.forEach((x) => {
+  const liEl = document.querySelectorAll('.content__boxBtn-item')
+  liEl.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      event.preventDefault()
+      const idButton = event.target.id
+      const bgWrap = document.querySelector('.wrapper')
       const sound = document.querySelector(`.${x.title}`)
       const range = document.querySelector('#range')
+      const iconWeather = document.querySelector(`.svg-${x.title}`)
       if (x.id === idButton) {
         // логика переключения изображения
         bgWrap.style.background = `url(${x.image})0 0 / cover no-repeat`
@@ -51,10 +48,10 @@ liEl.forEach((button) => {
         })
 
         // логика переключения иконки
-        iconWeather.classList.toggle('content__boxBtn-svg--show')
-        iconPause.classList.toggle('content__boxBtn-pause')
+        iconWeather.style.background = `url('public/assets/icons/pause.svg')0 0 / cover no-repeat`
       } else {
         sound.pause()
+        iconWeather.style.background = `url(${x.icon})0 0 / cover no-repeat`
       }
     })
   })
