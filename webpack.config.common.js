7ed672e3ae6c36f2deb342be727fb8ba41d4c35b
Vladimir Revyagin <skorabeyf@gmail.com>
2024-05-24 14:19:02 +0300

    feat(webpack): Базовая настройка TS

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -11,6 +11,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', 'jsx'],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve('./public/index.html'),
@@ -72,6 +75,11 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
 
         type: 'asset/resource',
+      },
+      {
+        test: /\.[tj]sx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
